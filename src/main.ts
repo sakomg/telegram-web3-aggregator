70fb090cb23a291f26cee91cf313127f0b0b37c9
Alexander Komegunov <a_komegunov@twistellar.com>
2024-05-23 14:09:44 +0300

    add server

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,11 @@
 import App from './app';
+import http from 'http';
 
-new App().bootstrap();
+http
+  .createServer(function (req: any, res: any) {
+    new App().bootstrap();
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('started server...');
+    res.end();
+  })
+  .listen(process.env.PORT || 8080);
